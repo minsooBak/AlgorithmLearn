5ba57436c69027046b32773037a1e67db54350c2
minsooBak <154481562+minsooBak@users.noreply.github.com>
2024-01-11 09:12:24 +0900

    [level unrated] Title: 몫 구하기, Time: 0.15 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120805. 몫 구하기/몫 구하기.cs
+++ 프로그래머스/unrated/120805. 몫 구하기/몫 구하기.cs
@@ -2,7 +2,7 @@ using System;
 
 public class Solution {
     public int solution(int num1, int num2) {
-        if(num1 < 0 && num1 > 100 && num2 < 0 && num2 > 100)
+        if( (num1 < 0 && num1 > 100) || (num2 < 0 && num2 > 100) )
             return 0;
         
         return num1 / num2;
