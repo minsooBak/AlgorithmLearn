61713ad13ffb9638208d836e63c101c0e32ab535
minsooBak <154481562+minsooBak@users.noreply.github.com>
2024-01-11 09:09:27 +0900

    [level unrated] Title: 숫자 비교하기, Time: 0.16 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120807. 숫자 비교하기/숫자 비교하기.cs
+++ 프로그래머스/unrated/120807. 숫자 비교하기/숫자 비교하기.cs
@@ -2,7 +2,6 @@ using System;
 
 public class Solution {
     public int solution(int num1, int num2) {
-        int answer = 0;
         if(num1 < 0 && num1 > 10000 && num2 < 0 && num2 > 10000)
             return 0;
         
