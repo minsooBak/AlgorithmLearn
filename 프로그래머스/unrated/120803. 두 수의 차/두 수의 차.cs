396848599c203649eea5e49f767e6bd611e4dca4
minsooBak <154481562+minsooBak@users.noreply.github.com>
2024-01-11 10:00:56 +0900

    [level unrated] Title: 두 수의 차, Time: 0.15 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120803. 두 수의 차/두 수의 차.cs
+++ 프로그래머스/unrated/120803. 두 수의 차/두 수의 차.cs
@@ -2,7 +2,7 @@ using System;
 
 public class Solution {
     public int solution(int num1, int num2) {
-        if(num1 < -50000 && num1 > 50000 || num2 < -50000 && num2 > 50000)
+        if( (num1 < -50000 || num1 > 50000) || (num2 < -50000 || num2 > 50000) )
             return 0;
         
         return num1 - num2;
