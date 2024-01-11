8182322d606626b5f440c1ac470cce5cf768fa8b
minsooBak <154481562+minsooBak@users.noreply.github.com>
2024-01-11 10:07:28 +0900

    [level unrated] Title: 나이 출력, Time: 0.14 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120820. 나이 출력/나이 출력.cs
+++ 프로그래머스/unrated/120820. 나이 출력/나이 출력.cs
@@ -2,7 +2,7 @@ using System;
 
 public class Solution {
     public int solution(int age) {
-        if(age <= 0 && age > 120)
+        if(age <= 0 || age > 120)
             return 0;
         
         return (2022 - age) + 1;
