02308f7b685369be5c9897a50728ec4bd3944e0b
minsooBak <154481562+minsooBak@users.noreply.github.com>
2024-01-11 09:55:56 +0900

    [level unrated] Title: 짝수의 합, Time: 0.17 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        if(n <= 0 && n > 1000)
+            return 0;
+        int size = n / 2;
+        n -= n % 2;
+        int answer = 0;
+        for(int i = 0; i <= size; i++)
+        {
+            answer += n - (i * 2);
+        }
+        
+        return answer;
+    }
+}
