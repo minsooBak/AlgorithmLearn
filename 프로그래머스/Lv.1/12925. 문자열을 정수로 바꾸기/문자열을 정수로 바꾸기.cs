886a506e5ab2c6977cdbb128957bd15db97382c3
Minsoo <154481562+minsooBak@users.noreply.github.com>
2024-01-13 13:27:07 +0900

    [level Lv.1] Title: 문자열을 정수로 바꾸기, Time: 0.75 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int solution(string s) {
+        var str = s.Split("-");
+        if(str.Length == 2)
+        {
+            return int.Parse(str[1]) * -1;
+        }
+        
+        return int.Parse(str[0]);
+    }
+}
