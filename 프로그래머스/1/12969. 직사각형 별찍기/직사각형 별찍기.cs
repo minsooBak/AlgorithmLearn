be86e7728268cec5c0b484babbd3a431bb288868
Minsoo <154481562+minsooBak@users.noreply.github.com>
2024-01-19 09:36:29 +0900

    [level 1] Title: 직사각형 별찍기, Time: 25.43 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.cs
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.cs
@@ -4,21 +4,20 @@ public class Example
 {
     public static void Main()
     {
-        String[] s;
-
         Console.Clear();
-        s = Console.ReadLine().Split(' ');
+        var s = Console.ReadLine().Split(' ');
 
         int a = Int32.Parse(s[0]);
         int b = Int32.Parse(s[1]);
         
+        string str = "";
+        for(int j = 0; j < a; j++)
+        {
+            str += "*";
+        }
         for(int i = 0; i < b; i++)
         {
-            for(int j = 0; j < a; j++)
-            {
-                Console.Write("*");
-            }
-            Console.WriteLine();
+            Console.WriteLine($"{str}");
         }
     }
 }
