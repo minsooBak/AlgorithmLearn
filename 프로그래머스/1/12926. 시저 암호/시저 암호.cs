fc1e0559c1a67eafe8b464198c49940755cafa27
Minsoo <154481562+minsooBak@users.noreply.github.com>
2024-01-30 09:23:03 +0900

    [level 1] Title: 시저 암호, Time: 40.50 ms, Memory: 47.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.cs
@@ -0,0 +1,30 @@
+using System;
+
+public class Solution {
+    public string solution(string s, int n) {
+        string answer = "";
+        foreach(char c in s)
+        {
+           if(c != ' ')
+           {
+               int temp = 0;
+               if(c <= 'Z')
+               {
+                   temp = c + n;
+                   if(temp > 'Z') temp = ('A' - 1) + (temp - 'Z');
+               }
+               else if(c <= 'z')
+               {
+                   temp = c + n;
+                   if(temp > 'z') temp = ('a' - 1) + (temp - 'z');
+               }
+               answer += Convert.ToChar(temp);
+           }
+            else
+            {
+                answer += ' ';
+            }
+        }
+        return answer;
+    }
+}
