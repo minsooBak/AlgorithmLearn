631dc7c353c7b4bbd9f940271102d3b40dc68a93
Minsoo <154481562+minsooBak@users.noreply.github.com>
2024-02-02 09:52:02 +0900

    [level 1] Title: K번째수, Time: 1.71 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cs
+++ 프로그래머스/1/42748. K번째수/K번째수.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 
 public class Solution {
     public int[] solution(int[] array, int[,] commands) {
-        int[] answer = new int[commands.Length / 3];
-        List<int> result = new List<int>(commands.Length / 3);
-        
+        int[] answer = new int[commands.GetLength(0)];
         for(int i = 0; i < commands.GetLength(0); i++)
         {
             if(commands[i, 1] - commands[i, 0] == 0)
@@ -13,6 +11,7 @@ public class Solution {
                 answer[i] = array[commands[i, 0] - 1];
             }else
             {
+                List<int> result = new List<int>(commands[i, 1] - commands[i, 0] + 1);
                 for(int j = commands[i, 0] - 1; j < commands[i, 1]; j++)
                 {
                     result.Add(array[j]);
