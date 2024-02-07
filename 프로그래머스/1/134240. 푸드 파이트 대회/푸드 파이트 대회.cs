f3d191d7a3ac64b8d724d330bbd585f824f31b1b
Minsoo <154481562+minsooBak@users.noreply.github.com>
2024-02-07 09:40:54 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 1.29 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Text;
+
+public class Solution {
+    
+    private const int MAXSIZE = 1000;
+    
+    public string solution(int[] food) {
+        string answer = "";
+        StringBuilder sb = new StringBuilder(MAXSIZE);
+        
+        for(int i = 1; i < food.Length; i++)
+        {
+            int sum = food[i] / 2;
+            if(sum > 0)
+            {
+                for(int j = 0; j < sum; j++)
+                {
+                    sb.Append(i);
+                }
+                answer += sb.ToString();
+                sb.Clear();
+            }
+        }
+        char[] str = answer.ToCharArray();
+        Array.Reverse(str);
+        answer = $"{answer}0{new string(str)}";
+        return answer;
+    }
+}
