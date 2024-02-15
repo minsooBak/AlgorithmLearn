02e78cfdd2c251424fddf0598778dbc53a39b8da
Minsoo <154481562+minsooBak@users.noreply.github.com>
2024-02-15 09:47:48 +0900

    [level 1] Title: 과일 장수, Time: 1.07 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.cs
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cs
@@ -3,7 +3,7 @@ using System;
 public class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-        Array.Sort(score, (a, b) => (a > b) ? -1 : 1);
+        Array.Sort(score, (a, b) => b.CompareTo(a));
         for(int i = 0; i <= score.Length - m; i+=m)
         {
             answer += score[i + (m - 1)] * m;
