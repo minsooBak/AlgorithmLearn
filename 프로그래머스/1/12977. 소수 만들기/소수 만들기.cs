e59f242df7021f89cc86a179c31283f23467e8e1
Minsoo <154481562+minsooBak@users.noreply.github.com>
2024-07-08 09:31:13 +0900

    [level 1] Title: 소수 만들기, Time: 0.22 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+
+class Solution
+{
+    public int solution(int[] nums)
+    {
+        int answer = 0;
+        int num_len = nums.Length;
+        for(int i = 0; i < num_len - 2; ++i)
+        {
+            int num1 = nums[i];
+            for(int j = i + 1; j < num_len - 1; ++j)
+            {
+                int sum = num1 + nums[j];
+                for(int k = j + 1; k < num_len; ++k)
+                {
+                    int result = sum + nums[k];
+                    bool isCheck = true;
+                    for(int r = 2; r < result / 2; ++r)
+                    {
+                       if(result % r == 0)
+                       {
+                           isCheck = false;
+                           break;
+                       }
+                    }
+                    
+                    if(isCheck)
+                        answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
